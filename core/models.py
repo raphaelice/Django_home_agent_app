be63af4d4d3cc7fdc1972e2635a72d027962aecf
Raphael Ezeigwe <iraphael1308@gmail.com>
2022-04-15 17:55:10 +0100

    modified House.pictures, added AboutView & its url, template files

--- core/models.py
+++ core/models.py
@@ -7,11 +7,22 @@ class Place(models.Model):
         return self.name
 
 class House(models.Model):
+
+    TYPE = (
+        ('SC', 'Self-contain'),
+        ('1RA', 'One room apartment'),
+        ('2RA', 'Two room apartment'),
+        ('3RA', 'Three room apartment'),
+        ('4RA', 'Four room apartment')
+    )
+
     name = models.CharField(max_length=250)
+    type = models.CharField(max_length=3, choices=TYPE)
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
-    pictures = models.ImageField(upload_to='images')
+    pictures = models.ImageField(upload_to='core/static/core/images')
     price = models.IntegerField()
-    description = models.TextField()
+    water = models.BooleanField(default=False)
+    occupied = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
--- core/views.py
+++ core/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, TemplateView
 from core.models import House
 
 class IndexView(ListView):
     model = House
-    template_name = ''
+    template_name = 'core/index.html'
 
 class DetailView(DetailView):
     model = House
-    template_name = ''
+    template_name = 'core/detail.html'
+
+class AboutView(TemplateView):
+    template_name = 'core/about.html'
