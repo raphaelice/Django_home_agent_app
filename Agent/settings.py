6be5ed77d2188eea5eb7401c4e8103af0b6b3544
Raphael Ezeigwe <iraphael1308@gmail.com>
2022-05-04 18:11:38 +0100

    add cascading image delete

--- Agent/settings.py
+++ Agent/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,6 +129,8 @@ STATIC_ROOT = BASE_DIR/'staticfiles'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+PARTNER_EMAIL = os.getenv('PARTNER_EMAIL')
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- core/models.py
+++ core/models.py
@@ -1,5 +1,12 @@
+import os
+
 from django.db import models
+from django.conf import settings
 from django.shortcuts import reverse
+from django.db.models.signals import post_delete, post_save
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.dispatch import receiver
 
 class Location(models.Model):
     name = models.CharField(max_length=120)
@@ -41,3 +48,15 @@ class Image(models.Model):
 
     def __str__(self):
         return f"{self.house.name} images"
+
+
+@receiver(post_delete, sender=Image)
+def delete_pictures(sender, instance, *args, **kwargs):
+    if instance.image:
+        os.remove(instance.image.path)
+
+@receiver(post_save, sender=House)
+def send_email(sender, instance, *args, **kwargs):
+    sender_email = User.objects.filter(is_superuser=True)[0].email
+    if not instance.vacant:
+        send_mail('Purchase Successful', 'A house has been bought!', sender_email, settings.PARTNER_EMAIL)
