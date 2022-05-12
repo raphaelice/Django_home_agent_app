d8c3468d5cf9a5e9f2d1aab0e25c1ef60a5d00d9
Raphael Ezeigwe <iraphael1308@gmail.com>
2022-05-12 17:34:31 +0100

    made some more changes to settings.py

--- Agent/settings.py
+++ Agent/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = ['127.0.0.1', 'dinels-agency.herokuapp.com']
 
